a9fdb49b626805feff143fb96ec0ebbcdfdd3eac
Stas Lyahnovich <stas.lyahnovich@gmail.com>
2012-05-14 00:27:11 +0400

    fixed

--- e1-29.scm
+++ e1-29.scm
@@ -1,7 +1,19 @@
-(define (sim-rule f a b n)
-  (define (sim-iter k h s)
-    (if (< (+ a (* k h)) b)
-        (sim-iter (+ k 1) h (+ s (* (if (even? k) 2 4) (f (+ a (* k h))))))
-        s))
-  (* (/ (/ (- b a) n) 3) (+ (f a) (f b) (sim-iter 1 (/ (- b a) n) 0))))
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a) (sum term (next a) next b))))
 
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (inc x) (+ x 1))
+  (define (y k) (f (+ a (* h k))))
+  (define (term k)
+    (* (cond ((or (= k 0) (= k n)) 1)
+            ((even? k) 2)
+            ((odd? k) 4)) (y k)))
+  (* (/ h 3) (sum term 0 inc n)))
+
+(define (even? x) (= 0 (remainder x 2)))
+(define (odd? x) (not (even? x)))
+
+(simpson cube 0 1 10.)
